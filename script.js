a61b70591b4c9cd802e4e63d8618203e1d72da10
ChomynNicolas <160611820+ChomynNicolas@users.noreply.github.com>
2024-02-24 15:41:54 -0300

    actualizacion final del dia 24 de febrero

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const CALCULAR = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
+const RES= document.getElementById('res');
 const MAN = document.getElementById('man');
 const SUPERFC = document.getElementById('opciones');
 
@@ -12,18 +13,22 @@ CALCULAR.addEventListener('click', () => {
         ERROR.style.display = 'none';
         let flujo = calcflu(DATO);
         let mant = flujo * 1.5;
-        FLU.innerHTML = flujo + ' cc/hr';
-        MAN.innerHTML = 'm+m/2 ' + mant + ' cc/hr';
+        RES.innerHTML = flujo + " Diario";    
+        FLU.innerHTML = Math.round(flujo/24) + ' cc/hr';
+        MAN.innerHTML = 'm+m/2 ' + Math.round(mant/24) + ' cc/hr';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
+        RES.style.display = 'block';
     }else {
         ERROR.style.display = 'none';
         let flujo = calcflu(DATO);
         let mant = flujo * 1.5;
-        FLU.innerHTML = flujo*1500 +' 0 '+flujo*2000 + ' cc/hr';
-        MAN.innerHTML = 'm+m/2 ' + Math.round(mant*1500)+' 0 '+Math.round(mant*2000) + ' cc/hr';
+        RES.innerHTML = flujo*1500 + ' o '+ flujo*2000 + " Diario";
+        FLU.innerHTML = Math.round(flujo*1500/24) +' o '+ Math.round(flujo*2000/24) + ' cc/hr';
+        MAN.innerHTML = 'm+m/2 ' + Math.round((mant*1500)/24)+' o '+Math.round((mant*2000)/24) + ' cc/hr';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
+        RES.style.display = 'block';
 
         
 
@@ -39,12 +44,12 @@ function calcflu(peso) {
     let num = peso;
     let flujo = 0;
     if (num < 31) {
-        if (num > 20) {
+        if (num >= 20) {
 
             aux = num - 20;
             flujo = 1500 + (aux * 20);
         }
-        if (num > 10) {
+        if (num >= 10) {
             aux = num - 10;
             flujo = 1000 + (aux * 50);
         } else { flujo = num * 100; }
